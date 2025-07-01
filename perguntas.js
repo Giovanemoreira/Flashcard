2af8497038a565b3e0d9e71a06cfb47b2a14475b
Giovanemoreira <giovane.demarque@escola.pr.gov.br>
2025-07-01 10:35:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Biotecnologia',
+    'O que é biotecnologia vermelha?',
+   ' A biotecnologia vermelha aborda assuntos voltados à saúde'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é biotecnologia amarela?',
+    'A biotecnologia amarela aborda assuntos voltados à produção de alimentos'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é biotecnologia dourada?',
+    'A biotecnologia dourada aborda assuntos voltado a nanotecnologia'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é biotecnologia roxa?',
+    A biotecnologia roxa aborda assuntos voltado a ética de todas as biotecnologia
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
