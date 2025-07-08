2011fa5f2688fbe2cdd03e9cf54e6062ef07855d
Giovanemoreira <giovane.demarque@escola.pr.gov.br>
2025-07-08 10:07:02 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,47 +1,23 @@
-criaCartao(
-    'Biotecnologia',
-    'O que é biotecnologia vermelha?',
-   ' A biotecnologia vermelha aborda assuntos voltados à saúde'
-)
+criaCartao( 
+'Biotecnologia 2.0', 
+'O que é biotecnologia marrom?', 
+'A biotecnologia marrom, ela conserva e recupera ambientes áridos, semiáridos e desertos' 
 
-criaCartao(
-    'Biotecnologia',
-    'O que é biotecnologia amarela?',
-    'A biotecnologia amarela aborda assuntos voltados à produção de alimentos'
-)
+) 
 
-criaCartao(
-    'Biotecnologia',
-    'O que é biotecnologia dourada?',
-    'A biotecnologia dourada aborda assuntos voltado a nanotecnologia'
-)
 
-criaCartao(
-    'Biotecnologia',
-    'O que é biotecnologia roxa?',
-   ' A biotecnologia roxa aborda assuntos voltado a ética de todas as biotecnologia'
-)
+criaCartao( 
+'Biotecnologia', 
+'O que é biotecnologia azul?', 
+'A biotecnologia azul aborda assunto da vida marinha' 
 
-criaCartao(
-    'Biotecnologia',
-    'O que é biotecnologia marrom?',
-    'A biotecnologia marrom, ela faz conserva anbientes aridos, semiáridos e desertos'
-)
-
-criaCartao(
-    'Biotecnologia',
-    'O que é biotecnologia azul?',
-    'A biotecnologia azul aborda assuntos da vida marinha'
-)
-
-criaCartao(
-    'Biotecnologia',
-    'O que é biotecnologia verde?',
-    'A biotecnologia verde aborda assuntos relacionados a alimentos transgênicos'
 )
 
 
 
+criaCartao( 
+'Biotecnologia', 
+'O que é biotecnologia verde?', 
+'A biotecnologia azul aborda assuntos relacionado aos produtos transgênicos' 
 
-
-
+)
